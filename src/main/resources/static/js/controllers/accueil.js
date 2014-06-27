61f824f9589046ffa0e75058cfef0322540182f0
bodtx <alexis.krier@laposte.net>
2014-06-27 11:39:41 +0200

    BootStrap power

--- src/main/resources/static/js/controllers/accueil.js
+++ src/main/resources/static/js/controllers/accueil.js
@@ -1,5 +1,5 @@
 
-var accueilControllers = angular.module('accueilControllers', []);
+var accueilControllers = angular.module('accueilControllers',['ui.bootstrap']);
 
 //accueil Hello
 accueilControllers.controller('AccueilCtrl', ['$scope', 'userNameService' , function ($scope, userNameService) {
