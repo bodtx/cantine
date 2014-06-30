eb33dfd5bd6c4c72284fd7e828653ab5e6fb1f39
Zorelien <dijoux.aurelien@gmail.com>
2014-06-30 14:36:38 +0200

    bootStrap power Page d'accueil

---
+++ src/main/resources/static/js/controllers/menuDuJour.js
@@ -0,0 +1,11 @@
+
+var menuDuJourControllers = angular.module('menuDuJourControllers',['ui.bootstrap']);
+
+//menu du Jour
+menuDuJourControllers.controller('MenuDuJourCtrl', ['$scope' , function ($scope) {
+
+	   //TBD
+
+
+}]);
+
