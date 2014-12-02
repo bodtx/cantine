4c3e47c6a09efdc632fd534628f439b9d586476b
Zorelien <dijoux.aurelien@gmail.com>
2014-12-02 17:17:48 +0100

    https://github.com/bodtx/cantine/issues/1 : Open Temptation

--- src/main/resources/static/js/controllers/badgeuse.js
+++ src/main/resources/static/js/controllers/badgeuse.js
@@ -3,7 +3,7 @@ var badgeuseControllers  = angular.module('badgeuseControllers', []);
 
 //TODO utiliser les ng-show?
 //calcule badgeuse
-badgeuseControllers.controller('HeureCtrl', function($scope, badgeInfoService) {
+badgeuseControllers.controller('HeureCtrl', function($scope, badgeInfoService, openTemptationService) {
 	badgeInfoService.getBadgeInfo().then(function(badgeInfo) {
 
 		//nb clic
@@ -59,5 +59,9 @@ badgeuseControllers.controller('HeureCtrl', function($scope, badgeInfoService) {
 
 
 	});
+
+    $scope.openTemptation = function(nom) {
+        openTemptationService.openTemptation();
+    }
 
 });
