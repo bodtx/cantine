17b196f8f117f36cb7eab91255f83ca99032c6f7
zorelien <dijoux.aurelien@gmail.com>
2016-07-19 10:36:30 +0200

    mdp Anais

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,7 +7,7 @@ create unique index ix_auth_username on authorities (username, authority);
 
 insert into users values ('CER3100441','$2a$10$lRHm7s/FphEyoYGDBcLn7eiLdyyOJV9q7cPXd0PzYXZ96mGLpLm7S',true);
 insert into users values ('CER3100307','$2a$10$fdSVQUS3DYkzrIl8HmbSgO1ak0pO8s.ihc63YGvFLNh.3r3aVboru',true);
-insert into users values ('CER3100444','$2a$10$BJ5sju4TsU.wu1xE4UJVbuaBW0donzEUax3WqvN5jX/zxgL8sX8we',true);
+insert into users values ('CER3100444','$2a$10$llcF2KjNnKTJiIfbCuAnnehiFunvGhtwngkVf3KUQpW3pzPcm1xjG',true);
 
 insert into authorities values ('CER3100441','USER');
 insert into authorities values ('CER3100307','USER');
