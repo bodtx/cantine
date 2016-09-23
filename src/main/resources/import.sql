20ac255dfc4395113df26522d959ac53c6cd31ed
Alexis KRIER <alexis.krier@cirso.fr>
2016-09-23 16:06:58 +0200

    update mp

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,7 +7,7 @@ create unique index ix_auth_username on authorities (username, authority);
 
 insert into users values ('CER3100441','$2a$10$dwho.3iD8eqhH3JbTxjUj.syogUtvSpOpSjm7XBRVPB/o7f4FuZLW',true);
 insert into users values ('CER3100307','$2a$10$R3Pa1UXWA9Cr5jDtoQPiveimiubTDc0Ud7sOLFyS6.K3p1Bd84RN2',true);
-insert into users values ('CER3100444','$2a$10$BJ5sju4TsU.wu1xE4UJVbuaBW0donzEUax3WqvN5jX/zxgL8sX8we',true);
+insert into users values ('CER3100444','$2a$10$llcF2KjNnKTJiIfbCuAnnehiFunvGhtwngkVf3KUQpW3pzPcm1xjG',true);
 
 
 insert into authorities values ('CER3100441','USER');
